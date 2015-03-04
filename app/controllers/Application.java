1ada26885f1b44d68f81da15c67e3612752225c4
Hesham Massoud <hesham.massoud@tum.de>
2015-03-04 23:02:52 +0100

    Create a cart and execute an order for a random customer and view the actual number of orders per customers and the order details.

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,13 +1,22 @@
 package controllers;
 
+import io.sphere.client.model.VersionedId;
+import io.sphere.client.shop.CreateOrderBuilder;
 import io.sphere.client.shop.SphereClient;
+import io.sphere.client.shop.model.Address;
+import io.sphere.client.shop.model.Cart;
+import io.sphere.client.shop.model.Cart.InventoryMode;
+import io.sphere.client.shop.model.CartUpdate;
 import io.sphere.client.shop.model.Customer;
 import io.sphere.client.shop.model.CustomerName;
 import io.sphere.client.shop.model.Order;
+import io.sphere.client.shop.model.PaymentState;
 import io.sphere.client.shop.model.Product;
 import views.html.*;
 import java.io.IOException;
+import java.util.Currency;
 import java.util.List;
+import java.util.Locale;
 
 import play.cache.Cache;
 import play.mvc.Controller;
@@ -28,9 +37,35 @@ public class Application extends Controller {
 
 		List<Order> allOrders = sphere.orders().all().fetch().getResults();
 		Cache.set("orders", allOrders);
+		
+		/*
+		 * Code used to create an order for a user
+		 * 
+		Customer customerOne = allCustomers.get(0);
+		Product lastProduct = allProducts.get(allProducts.size()-1);
+		Currency currency = Currency.getInstance("EUR");
+		
+		//sphere.carts().createCart(currency,customerOneId, Cart.InventoryMode.valueOf("ReserverOnOrder"));
+		
+		
+		//Cart cart = sphere.carts().createCart(currency,customerOne.getId(), InventoryMode.None).execute();
+		Cart cart = sphere.carts().forCustomer(customerOne.getId()).fetch().get();
+		
+		CartUpdate updateCart = new CartUpdate();
+		updateCart.addLineItem(2, lastProduct.getId());
+		
+		updateCart.setShippingAddress(customerOne.getAddresses().get(0));
+		cart = sphere.carts().updateCart(cart.getIdAndVersion(), updateCart).execute();
+		
+		CreateOrderBuilder orderBuilder = new CreateOrderBuilder(cart.getIdAndVersion(), PaymentState.Paid);
+		Order order = sphere.orders().createOrder(orderBuilder).execute();
+		
+		String totalPriceInCart = cart.getTotalPrice().toString();
+		*/
+		
 
 		sphere.shutdown();
-		return ok(index.render("Dashboard", "Demo"));
+		return ok(index.render("Dashboard", "Demo" + "-" + allOrders.size()));// + order.getOrderNumber() + "-" + order.getTotalQuantity() + "-"));
 	}
 
 }
--- app/controllers/Customers.java
+++ app/controllers/Customers.java
@@ -1,8 +1,11 @@
 package controllers;
 
+import io.sphere.client.shop.model.Cart;
 import io.sphere.client.shop.model.Customer;
+import io.sphere.client.shop.model.Order;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 import views.html.*;
 import play.cache.Cache;
@@ -13,7 +16,20 @@ public class Customers extends Controller {
 
 	public static Result index() throws IOException {
 		List<Customer> allCustomers = (List<Customer>) Cache.get("customers");
-		return ok(customers.render("Customers", allCustomers.size() + " customers", allCustomers));
+		List<Order> allOrders = (List<Order>) Cache.get("orders");
+		ArrayList<Integer> customerNumberOfOrders = new ArrayList<Integer>();
+		for (int i = 0; i < allCustomers.size(); i++) {
+			Customer customer = allCustomers.get(i);
+			String customerId = customer.getId();
+			customerNumberOfOrders.add(0);
+			for (Order order : allOrders) {
+				String orderCustomerId = order.getCustomerId();
+				if (orderCustomerId.equals(customerId))
+					customerNumberOfOrders.set(i, 1);
+			}
+		}
+		Customer randomCustomer = allCustomers.get(0);
+		return ok(customers.render("Customers", allCustomers.size()
+				+ " customers", allCustomers, customerNumberOfOrders));
 	}
-
 }
