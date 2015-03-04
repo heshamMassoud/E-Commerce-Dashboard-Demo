aad66e3267f5a82a41710f3fa9007a9004bfb01b
Hesham Massoud <hesham.massoud@tum.de>
2015-03-04 20:51:25 +0100

    Implement controller and view for showing the orders' details.

--- app/controllers/Customers.java
+++ app/controllers/Customers.java
@@ -4,17 +4,16 @@ import io.sphere.client.shop.model.Customer;
 
 import java.io.IOException;
 import java.util.List;
-
+import views.html.*;
 import play.cache.Cache;
 import play.mvc.Controller;
 import play.mvc.Result;
-import views.html.*;
 
 public class Customers extends Controller {
 
 	public static Result index() throws IOException {
 		List<Customer> allCustomers = (List<Customer>) Cache.get("customers");
-		return ok(customers.render("Customer", allCustomers.size() + " customers", allCustomers));
+		return ok(customers.render("Customers", allCustomers.size() + " customers", allCustomers));
 	}
 
 }
