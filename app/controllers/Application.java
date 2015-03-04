2024b6fc0c2900d45f8a5101758f2fcfd34ba033
Hesham Massoud <hesham.massoud@tum.de>
2015-03-04 11:50:18 +0100

    Setup api communication with my demo SPHERE IO project "dashboard-demo-94"

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,14 +1,26 @@
 package controllers;
 
-import play.*;
-import play.mvc.*;
+import io.sphere.client.shop.SphereClient;
+import io.sphere.client.shop.model.Customer;
+import io.sphere.client.shop.model.CustomerName;
 
-import views.html.*;
+import java.io.IOException;
+import java.util.List;
+
+import play.mvc.Controller;
+import play.mvc.Result;
 
 public class Application extends Controller {
 
-    public static Result index() {
-        return ok(index.render("Your new application is ready."));
-    }
+	public static Result index() throws IOException {
+		SphereClient sphere = SphereClient.create(sphereConfig.Config
+				.load("conf/application.conf"));
+		List<Customer> allCustomers = sphere.customers().all().fetch()
+				.getResults();
+		sphere.shutdown();
+		Customer customer = allCustomers.get(0);
+		CustomerName customerName = customer.getName();
+		return ok(views.html.index.render("Welcome, " + customerName));
+	}
 
 }
